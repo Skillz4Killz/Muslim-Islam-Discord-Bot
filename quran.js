16dafafc5b72caaa979648cb9047cbc2d4a1f2d7
WallyThePenguin <mansourwaleed04@gmail.com>
2018-11-27 17:36:59 -0500

    Finished fatiha

--- quran.js
+++ quran.js
@@ -14,19 +14,19 @@ module.exports = {
     },
     4: {
       text: 'Sovereign of the Day of Recompense.',
-      image: 'https://i.imgur.com/nnel4PH.png',
+      image: 'https://i.imgur.com/jzflLWx.png',
     },
     5: {
       text: 'It is You we worship and You we ask for help.',
-      image: 'https://i.imgur.com/nnel4PH.png',
+      image: 'https://i.imgur.com/GxRlmTA.png',
     },
     6: {
       text: 'Guide us to the straight path -',
-      image: 'https://i.imgur.com/nnel4PH.png',
+      image: 'https://i.imgur.com/B0VzCm0.png',
     },
     7: {
       text: 'The path of those upon whom You have bestowed favor, not of those who have evoked [Your] anger or of those who are astray.',
-      image: 'https://i.imgur.com/nnel4PH.png',
+      image: 'https://i.imgur.com/NYqFYnC.png',
     },
 
   }
