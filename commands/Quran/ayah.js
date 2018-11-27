a88ffe52db27b6fafb731bff381718cc9d077d46
Skillz4Killz <admin@ezlgg.com>
2018-11-27 18:08:04 -0500

    more work on ayah

--- commands/Quran/ayah.js
+++ commands/Quran/ayah.js
@@ -1,14 +1,11 @@
-const { Command } = require('klasa');
-const { MessageEmbed } = require('discord.js');
+const { Command } = require('klasa')
+const { MessageEmbed } = require('discord.js')
 
-const Quran = require('../../quran.js');
+const Quran = require('../../quran.js')
 
 module.exports = class extends Command {
-
-  constructor(...args) {
+  constructor (...args) {
     super(...args, {
-      enabled: true,
-      runIn: ['text', 'dm', 'group'],
       requiredPermissions: ['EMBED_LINKS'],
       aliases: ['a'],
       description: 'Read a specific ayah.',
@@ -17,29 +14,30 @@ module.exports = class extends Command {
       usageDelim: ' ',
       quotedStringSupport: true,
       subcommands: false
-    });
+    })
   }
 
-  async run(message, [surah, ayah]) {
+  async run (message, [surah, ayah]) {
     // Check if the surah provided is a number or a string
     if (surah) {
-      const surahIsString = isNaN(surah); 
-      if (surahIsString) surah = surahNameToInit(surah);
-    } else surah = this.getRandom();
+      const surahIsString = isNaN(surah)
+      // TODO: create this function
+      if (surahIsString) surah = this.surahNameToInit(surah)
+    } else surah = this.getRandom()
 
-    const ayahToSend = Quran[surah][ayah || this.getRandom(surah)];
+    const ayahToSend = Quran[surah][ayah || this.getRandom(surah)]
 
     return message.send(new MessageEmbed()
       .setColor('RANDOM')
+      .setAuthor(`Surah ${Quran[surah].name} Ayah #${ayah}`, message.author.displayAvatarURL())
       .setDescription(ayahToSend.text)
       .setImage(ayahToSend.image)
       .setFooter('Credits To Quran.com')
-    );
+    )
   }
 
-  getRandom(surah) {
+  getRandom (surah) {
     // TODO: once more surahs are added change the 1 at the end to 114
-    return 1 + Math.floor(Math.random() * surah ? Object.keys(Quran[surah]).length : 1);
+    return 1 + Math.floor(Math.random() * surah ? Object.keys(Quran[surah]).length : 1)
   }
-
-};
+}
