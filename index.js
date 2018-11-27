ea2cb3c304367bfb0718d4fa9733cc479078f123
Skillz4Killz <admin@ezlgg.com>
2018-11-27 16:37:32 -0500

    more default options enabled

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ new Client({
     fetchAllMembers: false,
     prefix: '!',
     commandEditing: true,
+    commandLogging: true,
+    noPrefixDM: true,
     typing: true,
     readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
 }).login(token);
