cffb0ac35258b95e093999fc36b44dfa4f9ea03b
Skillz <gankstarsleague@gmail.com>
2018-11-27 23:21:03 -0500

    adding total verse number for surah fatihah and its name

--- quran.js
+++ quran.js
@@ -1,33 +1,41 @@
 module.exports = {
   1: {
+    name: 'Al-Fatihah',
     1: {
       text: 'In the name of Allah, the Entirely Merciful, the Especially Merciful.',
       image: 'https://i.imgur.com/rYsiRl9.png',
+      verse: 1
     },
     2: {
       text: '[All] praise is [due] to Allah, Lord of the worlds -',
       image: 'https://i.imgur.com/7vnDFdV.png',
+      verse: 2
     },
     3: {
       text: 'The Entirely Merciful, the Especially Merciful,',
       image: 'https://i.imgur.com/nnel4PH.png',
+      verse: 3
     },
     4: {
       text: 'Sovereign of the Day of Recompense.',
       image: 'https://i.imgur.com/jzflLWx.png',
+      verse: 4
     },
     5: {
       text: 'It is You we worship and You we ask for help.',
       image: 'https://i.imgur.com/GxRlmTA.png',
+      verse: 5
     },
     6: {
       text: 'Guide us to the straight path -',
       image: 'https://i.imgur.com/B0VzCm0.png',
+      verse: 6
     },
     7: {
       text: 'The path of those upon whom You have bestowed favor, not of those who have evoked [Your] anger or of those who are astray.',
       image: 'https://i.imgur.com/NYqFYnC.png',
-    },
+      verse: 7
+    }
 
   }
-};
+}
