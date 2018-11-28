c5b90515de30a373852e0d8003ce0032345f808f
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2018-11-27 23:27:01 -0500

    Update quran.js

--- quran.js
+++ quran.js
@@ -42,43 +42,53 @@ module.exports = {
     name: 'Al-Baqarah',
     1: {
       text: 'Alif, Lam, Meem.',
-      image: 'https://i.imgur.com/oBPOSUv.png'
+      image: 'https://i.imgur.com/oBPOSUv.png',
+      verse: 8
     },
     2: {
       text: 'This is the Book about which there is no doubt, a guidance for those conscious of Allah -',
-      image: 'https://i.imgur.com/cWtOYM5.png'
+      image: 'https://i.imgur.com/cWtOYM5.png',
+      verse: 9
     },
     3: {
       text: 'Who believe in the unseen, establish prayer, and spend out of what We have provided for them,',
-      image: 'https://i.imgur.com/nnel4PH.png'
+      image: 'https://i.imgur.com/nnel4PH.png',
+      verse: 10
     },
     4: {
       text: 'And who believe in what has been revealed to you, [O Muhammad], and what was revealed before you, and of the Hereafter they are certain [in faith].',
-      image: 'https://i.imgur.com/JpI3L3B.png'
+      image: 'https://i.imgur.com/JpI3L3B.png',
+      verse: 11
     },
     5: {
       text: 'Those are upon [right] guidance from their Lord, and it is those who are the successful.',
-      image: 'https://i.imgur.com/uLcHlJj.png'
+      image: 'https://i.imgur.com/uLcHlJj.png',
+      verse: 12
     },
     6: {
       text: 'Indeed, those who disbelieve - it is all the same for them whether you warn them or do not warn them - they will not believe.',
-      image: 'https://i.imgur.com/WGt3hI6.png'
+      image: 'https://i.imgur.com/WGt3hI6.png',
+      verse: 13
     },
     7: {
       text: 'Allah has set a seal upon their hearts and upon their hearing, and over their vision is a veil. And for them is a great punishment.',
-      image: 'https://i.imgur.com/d5vABmg.png'
+      image: 'https://i.imgur.com/d5vABmg.png',
+      verse: 14
     },
     8: {
       text: 'And of the people are some who say, "We believe in Allah and the Last Day," but they are not believers.',
-      image: 'https://i.imgur.com/KfknUiE.png'
+      image: 'https://i.imgur.com/KfknUiE.png',
+      verse: 15
     },
     9: {
       text: 'They [think to] deceive Allah and those who believe, but they deceive not except themselves and perceive [it] not.',
-      image: 'https://i.imgur.com/ywRC9Xm.png'
+      image: 'https://i.imgur.com/ywRC9Xm.png',
+      verse: 16
     },
     10: {
       text: 'In their hearts is disease, so Allah has increased their disease; and for them is a painful punishment because they [habitually] used to lie.',
-      image: 'https://i.imgur.com/5MccCZN.png'
+      image: 'https://i.imgur.com/5MccCZN.png',
+      verse: 17
     }
   }
 }
