303dff02da532187d49754a006cccb43866bcc44
Skillz <gankstarsleague@gmail.com>
2018-12-05 17:51:54 -0500

    making an import file to keep imports clean

---
+++ imports.ts
@@ -0,0 +1,6 @@
+// Exporting necessary parts from dependencies
+export { Command, CommandStore, KlasaClient, Monitor, Task  } from 'klasa';
+export { Message, MessageEmbed } from 'discord.js';
+
+// Exporting project constant files
+export { default as Quran } from './quran.js';
