d6aa738691ee70837b95263f45be79975f4412ba
Skillz <gankstarsleague@gmail.com>
2018-12-05 13:47:43 -0500

    adding first draft of a salam monitor

---
+++ monitors/salam.js
@@ -0,0 +1,38 @@
+const { Monitor } = require('klasa')
+const salamTerms = [
+  'salam',
+  'asalamoalaikum wa rahmatullahi wa barakatuhu',
+  'asalamoalaikum'
+]
+
+const greetingTerms = [
+  'hi',
+  'hello',
+  'good morning',
+  'good day',
+  'good afternoon',
+  'good evening',
+  'good night',
+  'goodnight',
+  'gn'
+]
+
+module.exports = class extends Monitor {
+  constructor (...args) {
+    super(...args, {
+      ignoreOthers: false,
+      ignoreEdits: false
+    })
+  }
+
+  async run (message) {
+    // If this is not a in a guild cancel out
+    if (!message.guild || !message.content.length) return null
+    // if the message starts with a salam term reply
+    for (const term of salamTerms) if (message.content.startsWith(term)) return message.reply('Walaikumasalam Wa Rahmatullahi Wa Barakatuhu')
+    // if the message starts with non-salam greeting reply
+    for (const term of greetingTerms) if (message.content.startsWith(term)) return message.reply('Asalamoalaikum Wa Rahmatullahi Wa Barakatuhu')
+    // Cancel out
+    return null
+  }
+}
