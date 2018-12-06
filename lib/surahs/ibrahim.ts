e69e1b2508c59723233685daaa0800060d8ae796
Skillz <gankstarsleague@gmail.com>
2018-12-06 15:23:49 -0500

    another 4 surahs in TS

---
+++ lib/surahs/ibrahim.ts
@@ -0,0 +1,266 @@
+/* tslint:disable:object-literal-sort-keys */
+import { Ibrahim } from '../typings/islam';
+
+export const ibrahim: Ibrahim = {
+	ayah_1: {
+		text: 'Alif, Lam, Ra. [This is] a Book which We have revealed to you, [O Muhammad], that you might bring mankind out of darknesses into the light by permission of their Lord - to the path of the Exalted in Might, the Praiseworthy -',
+		image: '',
+		verse: 1620,
+	},
+	ayah_2: {
+		text: 'Allah, to whom belongs whatever is in the heavens and whatever is on the earth. And woe to the disbelievers from a severe punishment',
+		image: '',
+		verse: 1621,
+	},
+	ayah_3: {
+		text: 'The ones who prefer the worldly life over the Hereafter and avert [people] from the way of Allah, seeking to make it (seem) deviant. Those are in extreme error.',
+		image: '',
+		verse: 1622,
+	},
+	ayah_4: {
+		text: 'And We did not send any messenger except [speaking] in the language of his people to state clearly for them, and Allah sends astray [thereby] whom He wills and guides whom He wills. And He is the Exalted in Might, the Wise.',
+		image: '',
+		verse: 1623,
+	},
+	ayah_5: {
+		text: 'And We certainly sent Moses with Our signs, [saying], "Bring out your people from darknesses into the light and remind them of the days of Allah ." Indeed in that are signs for everyone patient and grateful.',
+		image: '',
+		verse: 1624,
+	},
+	ayah_6: {
+		text: 'And [recall, O Children of Israel], when Moses said to His people, "Remember the favor of Allah upon you when He saved you from the people of Pharaoh, who were afflicting you with the worst torment and were slaughtering your [newborn] sons and keeping your females alive. And in that was a great trial from your Lord.',
+		image: '',
+		verse: 1625,
+	},
+	ayah_7: {
+		text: "And [remember] when your Lord proclaimed, 'If you are grateful, I will surely increase you [in favor]; but if you deny, indeed, My punishment is severe.' \"",
+		image: '',
+		verse: 1626,
+	},
+	ayah_8: {
+		text: 'And Moses said, "If you should disbelieve, you and whoever is on the earth entirely - indeed, Allah is Free of need and Praiseworthy."',
+		image: '',
+		verse: 1627,
+	},
+	ayah_9: {
+		text: "Has there not reached you the news of those before you -  the people of Noah and 'Aad and Thamud and those after them? No one knows them but Allah . Their messengers brought them clear proofs, but they returned their hands to their mouths and said, \"Indeed, we disbelieve in that with which you have been sent, and indeed we are, about that to which you invite us, in disquieting doubt.\"",
+		image: '',
+		verse: 1628,
+	},
+	ayah_10: {
+		text: 'Their messengers said, "Can there be doubt about Allah, Creator of the heavens and earth? He invites you that He may forgive you of your sins, and He delays your death for a specified term." They said, "You are not but men like us who wish to avert us from what our fathers were worshipping.  So bring us a clear authority."',
+		image: '',
+		verse: 1629,
+	},
+	ayah_11: {
+		text: 'Their messengers said to them, "We are only men like you, but Allah confers favor upon whom He wills of His servants. It has never been for us to bring you evidence except by permission of Allah . And upon Allah let the believers rely.',
+		image: '',
+		verse: 1630,
+	},
+	ayah_12: {
+		text: 'And why should we not rely upon Allah while He has guided us to our [good] ways. And we will surely be patient against whatever harm you should cause us. And upon Allah let those who would rely [indeed] rely."',
+		image: '',
+		verse: 1631,
+	},
+	ayah_13: {
+		text: 'And those who disbelieved said to their messengers, "We will surely drive you out of our land, or you must return to our religion." So their Lord inspired to them, "We will surely destroy the wrongdoers.',
+		image: '',
+		verse: 1632,
+	},
+	ayah_14: {
+		text: 'And We will surely cause you to dwell in the land after them. That is for he who fears My position and fears My threat."',
+		image: '',
+		verse: 1633,
+	},
+	ayah_15: {
+		text: 'And they requested victory from Allah, and disappointed, [therefore], was every obstinate tyrant.',
+		image: '',
+		verse: 1634,
+	},
+	ayah_16: {
+		text: 'Before him is Hell, and he will be given a drink of purulent water.',
+		image: '',
+		verse: 1635,
+	},
+	ayah_17: {
+		text: 'He will gulp it but will hardly [be able to] swallow it. And death will come to him from everywhere, but he is not to die. And before him is a massive punishment.',
+		image: '',
+		verse: 1636,
+	},
+	ayah_18: {
+		text: 'The example of those who disbelieve in their Lord is [that] their deeds are like ashes which the wind blows forcefully on a stormy day; they are unable [to keep] from what they earned a [single] thing. That is what is extreme error.',
+		image: '',
+		verse: 1637,
+	},
+	ayah_19: {
+		text: 'Have you not seen that Allah created the heavens and the earth in truth? If He wills, He can do away with you and produce a new creation.',
+		image: '',
+		verse: 1638,
+	},
+	ayah_20: {
+		text: 'And that is not difficult for Allah .',
+		image: '',
+		verse: 1639,
+	},
+	ayah_21: {
+		text: 'And they will come out [for judgement] before Allah all together, and the weak will say to those who were arrogant, "Indeed, we were your followers, so can you avail us anything against the punishment of Allah ?" They will say, "If Allah had guided us, we would have guided you. It is all the same for us whether we show intolerance or are patient: there is for us no place of escape."',
+		image: '',
+		verse: 1640,
+	},
+	ayah_22: {
+		text: 'And Satan will say when the matter has been concluded, "Indeed, Allah had promised you the promise of truth. And I promised you, but I betrayed you. But I had no authority over you except that I invited you, and you responded to me. So do not blame me; but blame yourselves. I cannot be called to your aid, nor can you be called to my aid. Indeed, I deny your association of me [with Allah ] before. Indeed, for the wrongdoers is a painful punishment."',
+		image: '',
+		verse: 1641,
+	},
+	ayah_23: {
+		text: 'And those who believed and did righteous deeds will be admitted to gardens beneath which rivers flow, abiding eternally therein by permission of their Lord; and their greeting therein will be, "Peace!"',
+		image: '',
+		verse: 1642,
+	},
+	ayah_24: {
+		text: 'Have you not considered how Allah presents an example, [making] a good word like a good tree, whose root is firmly fixed and its branches [high] in the sky?',
+		image: '',
+		verse: 1643,
+	},
+	ayah_25: {
+		text: 'It produces its fruit all the time, by permission of its Lord. And Allah presents examples for the people that perhaps they will be reminded.',
+		image: '',
+		verse: 1644,
+	},
+	ayah_26: {
+		text: 'And the example of a bad word is like a bad tree, uprooted from the surface of the earth, not having any stability.',
+		image: '',
+		verse: 1645,
+	},
+	ayah_27: {
+		text: 'Allah keeps firm those who believe, with the firm word, in worldly life and in the Hereafter. And Allah sends astray the wrongdoers. And Allah does what He wills.',
+		image: '',
+		verse: 1646,
+	},
+	ayah_28: {
+		text: 'Have you not considered those who exchanged the favor of Allah for disbelief and settled their people [in] the home of ruin?',
+		image: '',
+		verse: 1647,
+	},
+	ayah_29: {
+		text: '[It is] Hell, which they will [enter to] burn, and wretched is the settlement.',
+		image: '',
+		verse: 1648,
+	},
+	ayah_30: {
+		text: 'And they have attributed to Allah equals to mislead [people] from His way. Say, "Enjoy yourselves, for indeed, your destination is the Fire."',
+		image: '',
+		verse: 1649,
+	},
+	ayah_31: {
+		text: '[O Muhammad], tell My servants who have believed to establish prayer and spend from what We have provided them, secretly and publicly, before a Day comes in which there will be no exchange, nor any friendships.',
+		image: '',
+		verse: 1650,
+	},
+	ayah_32: {
+		text: 'It is Allah who created the heavens and the earth and sent down rain from the sky and produced thereby some fruits as provision for you and subjected for you the ships to sail through the sea by His command and subjected for you the rivers.',
+		image: '',
+		verse: 1651,
+	},
+	ayah_33: {
+		text: 'And He subjected for you the sun and the moon, continuous [in orbit], and subjected for you the night and the day.',
+		image: '',
+		verse: 1652,
+	},
+	ayah_34: {
+		text: 'And He gave you from all you asked of Him. And if you should count the favor of Allah, you could not enumerate them. Indeed, mankind is [generally] most unjust and ungrateful.',
+		image: '',
+		verse: 1653,
+	},
+	ayah_35: {
+		text: 'And [mention, O Muhammad], when Abraham said, "My Lord, make this city [Makkah] secure and keep me and my sons away from worshipping idols.',
+		image: '',
+		verse: 1654,
+	},
+	ayah_36: {
+		text: 'My Lord, indeed they have led astray many among the people. So whoever follows me - then he is of me; and whoever disobeys me -  indeed, You are [yet] Forgiving and Merciful.',
+		image: '',
+		verse: 1655,
+	},
+	ayah_37: {
+		text: 'Our Lord, I have settled some of my descendants in an uncultivated valley near Your sacred House, our Lord, that they may establish prayer. So make hearts among the people incline toward them and provide for them from the fruits that they might be grateful.',
+		image: '',
+		verse: 1656,
+	},
+	ayah_38: {
+		text: 'Our Lord, indeed You know what we conceal and what we declare, and nothing is hidden from Allah on the earth or in the heaven.',
+		image: '',
+		verse: 1657,
+	},
+	ayah_39: {
+		text: 'Praise to Allah, who has granted to me in old age Ishmael and Isaac. Indeed, my Lord is the Hearer of supplication.',
+		image: '',
+		verse: 1658,
+	},
+	ayah_40: {
+		text: 'My Lord, make me an establisher of prayer, and [many] from my descendants. Our Lord, and accept my supplication.',
+		image: '',
+		verse: 1659,
+	},
+	ayah_41: {
+		text: 'Our Lord, forgive me and my parents and the believers the Day the account is established."',
+		image: '',
+		verse: 1660,
+	},
+	ayah_42: {
+		text: 'And never think that Allah is unaware of what the wrongdoers do. He only delays them for a Day when eyes will stare [in horror].',
+		image: '',
+		verse: 1661,
+	},
+	ayah_43: {
+		text: 'Racing ahead, their heads raised up, their glance does not come back to them, and their hearts are void.',
+		image: '',
+		verse: 1662,
+	},
+	ayah_44: {
+		text: 'And, [O Muhammad], warn the people of a Day when the punishment will come to them and those who did wrong will say, "Our Lord, delay us for a short term; we will answer Your call and follow the messengers." [But it will be said], "Had you not sworn, before, that for you there would be no cessation?',
+		image: '',
+		verse: 1663,
+	},
+	ayah_45: {
+		text: 'And you lived among the dwellings of those who wronged themselves, and it had become clear to you how We dealt with them. And We presented for you [many] examples."',
+		image: '',
+		verse: 1664,
+	},
+	ayah_46: {
+		text: 'And they had planned their plan, but with Allah is [recorded] their plan, even if their plan had been [sufficient] to do away with the mountains.',
+		image: '',
+		verse: 1665,
+	},
+	ayah_47: {
+		text: 'So never think that Allah will fail in His promise to His messengers. Indeed, Allah is Exalted in Might and Owner of Retribution.',
+		image: '',
+		verse: 1666,
+	},
+	ayah_48: {
+		text: '[It will be] on the Day the earth will be replaced by another earth, and the heavens [as well], and all creatures will come out before Allah, the One, the Prevailing.',
+		image: '',
+		verse: 1667,
+	},
+	ayah_49: {
+		text: 'And you will see the criminals that Day bound together in shackles,',
+		image: '',
+		verse: 1668,
+	},
+	ayah_50: {
+		text: 'Their garments of liquid pitch and their faces covered by the Fire.',
+		image: '',
+		verse: 1669,
+	},
+	ayah_51: {
+		text: 'So that Allah will recompense every soul for what it earned. Indeed, Allah is swift in account.',
+		image: '',
+		verse: 1670,
+	},
+	ayah_52: {
+		text: "This [Qur'an] is notification for the people that they may be warned thereby and that they may know that He is but one God and that those of understanding will be reminded.",
+		image: '',
+		verse: 1671,
+	},
+	name: 'Ibrahim',
+};
---
+++ lib/surahs/rad.ts
@@ -0,0 +1,171 @@
+/* tslint:disable:object-literal-sort-keys */
+import { Rad } from '../typings/islam';
+
+export const rad: Rad = {
+	ayah_1: {
+		text: 'For each one are successive [angels] before and behind him who protect him by the decree of Allah .  Indeed, Allah will not change the condition of a people until they change what is in themselves. And when Allah intends for a people ill, there is no repelling it.  And there is not for them besides Him any patron.',
+		image: '',
+		verse: 1587,
+	},
+	ayah_2: {
+		text: 'It is He who shows you lightening, [causing] fear and aspiration, and generates the heavy clouds.',
+		image: '',
+		verse: 1588,
+	},
+	ayah_3: {
+		text: 'And the thunder exalts [ Allah ] with praise of Him -  and the angels [as well] from fear of Him -  and He sends thunderbolts and strikes therewith whom He wills while they dispute about Allah ; and He is severe in assault.',
+		image: '',
+		verse: 1589,
+	},
+	ayah_4: {
+		text: 'To Him [alone] is the supplication of truth.  And those they call upon besides Him do not respond to them with a thing, except as one who stretches his hands toward water [from afar, calling it] to reach his mouth, but it will not reach it [thus]. And the supplication of the disbelievers is not but in error [i.e. futility].',
+		image: '',
+		verse: 1590,
+	},
+	ayah_5: {
+		text: 'And to Allah prostrates whoever is within the heavens and the earth, willingly or by compulsion, and their shadows [as well] in the mornings and the afternoons.',
+		image: '',
+		verse: 1591,
+	},
+	ayah_6: {
+		text: 'Say, "Who is Lord of the heavens and earth?" Say, " Allah ." Say, "Have you then taken besides Him allies not possessing [even] for themselves any benefit or any harm?" Say, "Is the blind equivalent to the seeing? Or is darkness equivalent to light? Or have they attributed to Allah partners who created like His creation so that the creation [of each] seemed similar to them?" Say, " Allah is the Creator of all things, and He is the One, the Prevailing."',
+		image: '',
+		verse: 1592,
+	},
+	ayah_7: {
+		text: 'He sends down from the sky, rain, and valleys flow according to their capacity, and the torrent carries a rising foam. And from that [ore] which they heat in the fire, desiring adornments and utensils, is a foam like it. Thus Allah presents [the example of] truth and falsehood. As for the foam, it vanishes, [being] cast off; but as for that which benefits the people, it remains on the earth. Thus does Allah present examples.',
+		image: '',
+		verse: 1593,
+	},
+	ayah_8: {
+		text: 'For those who have responded to their Lord is the best [reward], but those who did not respond to Him -  if they had all that is in the earth entirely and the like of it with it, they would [attempt to] ransom themselves thereby. Those will have the worst account, and their refuge is Hell, and wretched is the resting place.',
+		image: '',
+		verse: 1594,
+	},
+	ayah_9: {
+		text: 'Then is he who knows that what has been revealed to you from your Lord is the truth like one who is blind? They will only be reminded who are people of understanding -',
+		image: '',
+		verse: 1595,
+	},
+	ayah_10: {
+		text: 'Those who fulfill the covenant of Allah and do not break the contract,',
+		image: '',
+		verse: 1596,
+	},
+	ayah_11: {
+		text: 'And those who join that which Allah has ordered to be joined and fear their Lord and are afraid of the evil of [their] account,',
+		image: '',
+		verse: 1597,
+	},
+	ayah_12: {
+		text: 'And those who are patient, seeking the countenance of their Lord, and establish prayer and spend from what We have provided for them secretly and publicly and prevent evil with good -  those will have the good consequence of [this] home -',
+		image: '',
+		verse: 1598,
+	},
+	ayah_13: {
+		text: 'Gardens of perpetual residence; they will enter them with whoever were righteous among their fathers, their spouses and their descendants. And the angels will enter upon them from every gate, [saying],',
+		image: '',
+		verse: 1599,
+	},
+	ayah_14: {
+		text: '"Peace be upon you for what you patiently endured. And excellent is the final home."',
+		image: '',
+		verse: 1600,
+	},
+	ayah_15: {
+		text: 'But those who break the covenant of Allah after contracting it and sever that which Allah has ordered to be joined and spread corruption on earth -  for them is the curse, and they will have the worst home.',
+		image: '',
+		verse: 1601,
+	},
+	ayah_16: {
+		text: 'Allah extends provision for whom He wills and restricts [it]. And they rejoice in the worldly life, while the worldly life is not, compared to the Hereafter, except [brief] enjoyment.',
+		image: '',
+		verse: 1602,
+	},
+	ayah_17: {
+		text: 'And those who disbelieved say, "Why has a sign not been sent down to him from his Lord?" Say, [O Muhammad], "Indeed, Allah leaves astray whom He wills and guides to Himself whoever turns back [to Him] -',
+		image: '',
+		verse: 1603,
+	},
+	ayah_18: {
+		text: 'Those who have believed and whose hearts are assured by the remembrance of Allah . Unquestionably, by the remembrance of Allah hearts are assured."',
+		image: '',
+		verse: 1604,
+	},
+	ayah_19: {
+		text: 'Those who have believed and done righteous deeds -  a good state is theirs and a good return.',
+		image: '',
+		verse: 1605,
+	},
+	ayah_20: {
+		text: 'Thus have We sent you to a community before which [other] communities have passed on so you might recite to them that which We revealed to you, while they disbelieve in the Most Merciful. Say, "He is my Lord; there is no deity except Him. Upon Him I rely, and to Him is my return."',
+		image: '',
+		verse: 1606,
+	},
+	ayah_21: {
+		text: "And if there was any qur'an by which the mountains would be removed or the earth would be broken apart or the dead would be made to speak, [it would be this Qur'an], but to Allah belongs the affair entirely. Then have those who believed not accepted that had Allah willed, He would have guided the people, all of them? And those who disbelieve do not cease to be struck, for what they have done, by calamity -  or it will descend near their home -  until there comes the promise of Allah . Indeed, Allah does not fail in [His] promise.",
+		image: '',
+		verse: 1607,
+	},
+	ayah_22: {
+		text: 'And already were [other] messengers ridiculed before you, and I extended the time of those who disbelieved; then I seized them, and how [terrible] was My penalty.',
+		image: '',
+		verse: 1608,
+	},
+	ayah_23: {
+		text: 'Then is He who is a maintainer of every soul, [knowing] what it has earned, [like any other]?  But to Allah they have attributed partners. Say, "Name them. Or do you inform Him of that which He knows not upon the earth or of what is apparent of speech?" Rather, their [own] plan has been made attractive to those who disbelieve, and they have been averted from the way. And whomever Allah leaves astray -  there will be for him no guide.',
+		image: '',
+		verse: 1609,
+	},
+	ayah_24: {
+		text: 'For them will be punishment in the life of [this] world, and the punishment of the Hereafter is more severe. And they will not have from Allah any protector.',
+		image: '',
+		verse: 1610,
+	},
+	ayah_25: {
+		text: 'The example of Paradise, which the righteous have been promised, is [that] beneath it rivers flow. Its fruit is lasting, and its shade. That is the consequence for the righteous, and the consequence for the disbelievers is the Fire.',
+		image: '',
+		verse: 1611,
+	},
+	ayah_26: {
+		text: 'And [the believers among] those to whom We have given the [previous] Scripture rejoice at what has been revealed to you, [O Muhammad], but among the [opposing] factions are those who deny part of it. Say, "I have only been commanded to worship Allah and not associate [anything] with Him. To Him I invite, and to Him is my return."',
+		image: '',
+		verse: 1612,
+	},
+	ayah_27: {
+		text: 'And thus We have revealed it as an Arabic legislation. And if you should follow their inclinations after what has come to you of knowledge, you would not have against Allah any ally or any protector.',
+		image: '',
+		verse: 1613,
+	},
+	ayah_28: {
+		text: 'And We have already sent messengers before you and assigned to them wives and descendants. And it was not for a messenger to come with a sign except by permission of Allah . For every term is a decree.',
+		image: '',
+		verse: 1614,
+	},
+	ayah_29: {
+		text: 'Allah eliminates what He wills or confirms, and with Him is the Mother of the Book.',
+		image: '',
+		verse: 1615,
+	},
+	ayah_30: {
+		text: 'And whether We show you part of what We promise them or take you in death, upon you is only the [duty of] notification, and upon Us is the account.',
+		image: '',
+		verse: 1616,
+	},
+	ayah_31: {
+		text: 'Have they not seen that We set upon the land, reducing it from its borders?  And Allah decides; there is no adjuster of His decision. And He is swift in account.',
+		image: '',
+		verse: 1617,
+	},
+	ayah_32: {
+		text: 'And those before them had plotted, but to Allah belongs the plan entirely. He knows what every soul earns, and the disbelievers will know for whom is the final home.',
+		image: '',
+		verse: 1618,
+	},
+	ayah_33: {
+		text: 'And those who have disbelieved say, "You are not a messenger." Say, [O Muhammad], "Sufficient is Allah as Witness between me and you, and [the witness of] whoever has knowledge of the Scripture."',
+		image: '',
+		verse: 1619,
+	},
+	name: 'Ar-Ra\'d',
+};
