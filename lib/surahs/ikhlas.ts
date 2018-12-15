d8570bc5a5248fd4e416d7fac6f2cc726828af5e
WallyThePenguin <mansourwaleed04@gmail.com>
2018-12-15 17:24:55 -0500

    Deleted what skillz has done, added 114-105

---
+++ lib/surahs/ikhlas.ts
@@ -0,0 +1,12 @@
+import { Ikhlas } from '../types/islam';
+export const ikhlas: Ikhlas = {
+  ayah_1: { image: '', text: 'Say, "He is Allah, [who is] One,', verse: 6091 },
+  ayah_2: { image: '', text: 'Allah, the Eternal Refuge.', verse: 6092 },
+  ayah_3: { image: '', text: 'He neither begets nor is born,', verse: 6093 },
+  ayah_4: {
+    image: '',
+    text: 'Nor is there to Him any equivalent."',
+    verse: 6094,
+  },
+  name: 'Al-Ikhlas',
+};
---
+++ lib/surahs/quraysh.ts
@@ -0,0 +1,25 @@
+import { Quraysh } from '../types/islam';
+export const quraysh: Quraysh = {
+  ayah_1: {
+    image: '',
+    text: 'For the accustomed security of the Quraysh -',
+    verse: 6063,
+  },
+  ayah_2: {
+    image: '',
+    text: 'Their accustomed security [in] the caravan of winter and summer -',
+    verse: 6064,
+  },
+  ayah_3: {
+    image: '',
+    text: 'Let them worship the Lord of this House,',
+    verse: 6065,
+  },
+  ayah_4: {
+    image: '',
+    text:
+      'Who has fed them, [saving them] from hunger and made them safe, [saving them] from fear.',
+    verse: 6066,
+  },
+  name: 'Al-Quraysh',
+};
