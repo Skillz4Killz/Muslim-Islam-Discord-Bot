a0bb83242eba0147e4806282dfe17905712eac6d
WallyThePenguin <mansourwaleed04@gmail.com>
2018-12-16 00:43:54 -0500

    Now up to surah 90, Balad, Finished.  However encountered(and fixed) an issue, always on ayah 10 it becomes a tslint error, "alphabatecally incorrect", To fix this, i put an extra line between 9 and 10.

---
+++ lib/surahs/asr.ts
@@ -0,0 +1,12 @@
+import { Asr } from '../types/islam';
+export const asr: Asr = {
+  ayah_1: { image: '', text: 'By time,', verse: 6046 },
+  ayah_2: { image: '', text: 'Indeed, mankind is in loss,', verse: 6047 },
+  ayah_3: {
+    image: '',
+    text:
+      'Except for those who have believed and done righteous deeds and advised each other to truth and advised each other to patience.',
+    verse: 6048,
+  },
+  name: "Al-'Asr",
+};
