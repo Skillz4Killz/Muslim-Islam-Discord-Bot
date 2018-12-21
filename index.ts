b719912e78af1e820637cecc2f131912941da67a
WallyThePenguin <mansourwaleed04@gmail.com>
2018-12-21 13:32:05 -0500

    added .env support, replaced config, added dotenv typings, added dotenv.

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-import { Configs, KlasaClient } from './imports';
+import * as dotenv from 'dotenv';
+import { KlasaClient } from './imports';
+
+dotenv.config();
+let path;
+switch (process.env.NODE_ENV) {
+  case 'test':
+    path = `${__dirname}/../../.env.test`;
+    break;
+  case 'production':
+    path = `${__dirname}/../../.env.production`;
+    break;
+  default:
+    path = `${__dirname}/../../.env.development`;
+}
+dotenv.config({ path });
+export const token = process.env.token;
 
 KlasaClient.defaultUserSchema
   .add('reminders', (reminder) => reminder
@@ -16,4 +32,4 @@ new KlasaClient({
   prefix: '!',
   readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`,
   typing: true,
-}).login(Configs.token);
+}).login(process.env.token);
