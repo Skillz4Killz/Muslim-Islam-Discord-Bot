24b8832df1d5d8f28713b0a4e891c7df267d5476
Skillz <gankstarsleague@gmail.com>
2019-01-01 22:49:50 -0500

    adding image links for surah fatihah

--- lib/surahs/fatihah.ts
+++ lib/surahs/fatihah.ts
@@ -2,38 +2,38 @@ import { Fatihah } from '../types/islam';
 
 const fatihah: Fatihah = {
   ayah_1: {
-    image: '',
+    image: 'https://i.imgur.com/rYsiRl9.png',
     text:
       'In the name of Allah, the Entirely Merciful, the Especially Merciful.',
     verse: 1,
   },
   ayah_2: {
-    image: '',
+    image: 'https://i.imgur.com/7vnDFdV.png',
     text: '[All] praise is [due] to Allah, Lord of the worlds -',
     verse: 2,
   },
   ayah_3: {
-    image: '',
+    image: 'https://i.imgur.com/nnel4PH.png',
     text: 'The Entirely Merciful, the Especially Merciful,',
     verse: 3,
   },
   ayah_4: {
-    image: '',
+    image: 'https://i.imgur.com/jzflLWx.png',
     text: 'Sovereign of the Day of Recompense.',
     verse: 4,
   },
   ayah_5: {
-    image: '',
+    image: 'https://i.imgur.com/GxRlmTA.png',
     text: 'It is You we worship and You we ask for help.',
     verse: 5,
   },
   ayah_6: {
-    image: '',
+    image: 'https://i.imgur.com/B0VzCm0.png',
     text: 'Guide us to the straight path -',
     verse: 6,
   },
   ayah_7: {
-    image: '',
+    image: 'https://i.imgur.com/NYqFYnC.png',
     text:
       'The path of those upon whom You have bestowed favor, not of those who have evoked [Your] anger or of those who are astray.',
     verse: 7,
