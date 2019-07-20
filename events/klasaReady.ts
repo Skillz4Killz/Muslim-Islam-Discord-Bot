d677b83f1ee3afaae208907d322647ddeca719bb
Skillz <gankstarsleague@gmail.com>
2019-07-20 18:30:40 -0400

    prettier

--- events/klasaReady.ts
+++ events/klasaReady.ts
@@ -4,18 +4,20 @@ const tasksToCreate = [
   { name: 'reminder', time: '*/10 * * * *' },
   { name: 'changeProfilePicture', time: '0 */2 * * *' },
   // { name: 'fridaySurahKahf', time: '@weekly' }
-
-]
+];
 export default class extends Event {
   async run() {
     // For every task we will create them as needed
-    for (const task of tasksToCreate) await this.handleTaskCreation(task.name, task.time);
+    for (const task of tasksToCreate)
+      await this.handleTaskCreation(task.name, task.time);
 
     return this.client.emit('log', 'Klasa Ready Completed!');
   }
 
   handleTaskCreation(name: string, time: string) {
-    const taskExists = this.client.schedule.tasks.find((task) => task.taskName === name);
+    const taskExists = this.client.schedule.tasks.find(
+      (task) => task.taskName === name
+    );
     if (!taskExists) return this.client.schedule.create(name, time);
   }
 }
