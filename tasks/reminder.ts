785762103150eaf83e4594f24272577ab0795197
Skillz <gankstarsleague@gmail.com>
2019-07-21 15:11:38 -0400

    updating reminder to work on different servers

--- tasks/reminder.ts
+++ tasks/reminder.ts
@@ -1,67 +1,79 @@
-import { Message, MessageEmbed, Quran, Task, TextChannel } from '../imports';
+import { Message, MessageEmbed, Quran, Task } from '../imports';
+import { GuildSettings } from '../lib/types/settings/GuildSettings';
+import { UserSettings } from '../lib/types/settings/UserSettings';
 
 export default class extends Task {
-  private reminderChannel!: TextChannel;
 
   async run() {
     this.client.emit('log', 'Reminder Task Running');
-    // Find all users that have enabled the reminders
-    const usersToRemind = this.client.users
-      .filter((user) => user.settings.get('reminders.finishMonthly.enabled') as boolean)
-      .array();
-    // If no users canncel
-    if (!usersToRemind.length) return;
-    // Run a loop for each user
-    for (const user of usersToRemind) {
-      // Get the verse for this user or if it has none set it to 1
-      const verse = user.settings.get('reminders.finishMonthly.verse') as number || 1;
-      // Get the surah and ayah numbers to send based on the verse they are up to
-      let surah;
-      let ayah = 'ayah_1';
 
-      for (const surahKey of Object.keys(Quran)) {
-        const surahValue = Quran[surahKey];
-        for (const ayahKey of Object.keys(surahValue)) {
-          if (surahValue[ayahKey].verse !== verse) continue;
-          surah = surahValue;
-          ayah = ayahKey;
+    for (const guild of this.client.guilds.values()) {
+      const [channel] = await guild.settings.resolve(
+        GuildSettings.FinishMonthlyChannelID
+      );
+      if (!channel) continue;
+
+      // This channel has a valid reminder channel so fetch all members
+      await guild.members.fetch();
+
+      for (const member of guild.members.values()) {
+        // For each member get the status and verse of their quran reminders
+        const [enabled, nextVerse] = member.user.settings.pluck(
+          UserSettings.FinishMonthlyEnabled,
+          UserSettings.FinishMonthlyVerse
+        );
+
+        if (!enabled) continue;
+
+        // Get the surah and ayah numbers to send based on the verse they are up to
+        const verse = nextVerse || 1;
+        let surah;
+        let ayah = 'ayah_1';
+
+        for (const surahKey of Object.keys(Quran)) {
+          const surahValue = Quran[surahKey];
+          for (const ayahKey of Object.keys(surahValue)) {
+            if (surahValue[ayahKey].verse !== verse) continue;
+            surah = surahValue;
+            ayah = ayahKey;
+          }
         }
+        // Get the ayah object
+        const ayahToSend = surah[ayah];
+        // Create the embed to send
+        const embed = new MessageEmbed()
+          .setColor('RANDOM')
+          .setAuthor(
+            `Surah ${surah.name} Ayah #${ayah.substring(5)}`,
+            member.user.displayAvatarURL()
+          )
+          .setDescription(ayahToSend.text)
+          .setImage(ayahToSend.image)
+          .setTimestamp()
+          .setFooter('Credits To Quran.com');
+
+        // Send the reminder embed for the verse
+        const sentReminder = await channel.send(member.user, {
+          embed,
+        }) as Message;
+        // Add a reaction so the user can confirm they read it
+        if (sentReminder) await sentReminder.react('✅');
+        // Add 1 to the verse number or reset it if they are complete
+        await member.user.settings.update(
+          UserSettings.FinishMonthlyVerse,
+          verse === 6105 ? 1 : verse + 1,
+          { throwOnError: true }
+        );
+        // Log it
+        this.client.emit(
+          'log',
+          `Reminded ${member.user.tag} of ${
+            surah.name
+          } ${ayah} in the reminder task.`
+        );
       }
-      // Get the ayah object
-      const ayahToSend = surah[ayah];
-
-      const embed = new MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor(
-          `Surah ${surah.name} Ayah #${ayah.substring(5)}`,
-          user.displayAvatarURL()
-        )
-        .setDescription(ayahToSend.text)
-        .setImage(ayahToSend.image)
-        .setTimestamp()
-        .setFooter('Credits To Quran.com');
-
-      const channel = this.reminderChannel;
-      const sentReminder = (await channel.send(user, { embed })) as Message;
-      if (sentReminder) await sentReminder.react('✅');
-
-      await user.settings.update(
-        'reminders.finishMonthly.verse',
-        verse === 6105 ? 1 : verse + 1,
-        { throwOnError: true }
-      );
-
-      this.client.emit(
-        'log',
-        `Reminded ${user.tag} of ${surah.name} ${ayah} in the reminder task.`
-      );
     }
-    return;
-  }
 
-  async init() {
-    this.reminderChannel = this.client.channels.get(
-      '517175884432801795'
-    ) as TextChannel;
+    return;
   }
 }
