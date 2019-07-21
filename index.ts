c250e156adc5fd4b38e87f8c424afb76f42eaa13
Skillz <gankstarsleague@gmail.com>
2019-07-21 14:22:56 -0400

    custom perm levels and fix schemas

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import { PermissionLevels } from 'klasa';
 import { KlasaClient } from './imports';
 
 dotenv.config();
@@ -15,30 +16,41 @@ switch (process.env.NODE_ENV) {
 }
 dotenv.config({ path });
 
-KlasaClient.defaultUserSchema
-  .add('reminders', (reminder) => reminder
-    .add('finishMonthly', (monthly) => monthly
-      .add('verse', 'integer', { default: 1 })
-      .add('enabled', 'boolean')
-      .add('guildID', 'guild')
+KlasaClient.defaultUserSchema.add('reminders', (reminder) =>
+  reminder
+    .add('finishMonthly', (monthly) =>
+      monthly
+        .add('verse', 'integer', { default: 1 })
+        .add('enabled', 'boolean')
+        .add('guildID', 'guild')
     )
     .add('fridaySurahKahfEnabled', 'boolean')
-  );
+);
 
-KlasaClient.defaultGuildSchema
-  .add('reminders', (reminder) => reminder
+KlasaClient.defaultGuildSchema.add('reminders', (reminder) =>
+  reminder
     .add('fridaySurahKahfChannelID', 'textchannel')
     .add('finishMonthlyChannelID', 'textchannel')
-  );
+);
 
 const Muslim = new KlasaClient({
   commandEditing: true,
   commandLogging: true,
   fetchAllMembers: false,
   noPrefixDM: true,
+  permissionLevels: new PermissionLevels()
+    .add(0, () => true)
+    .add(1, (message) => message.member.hasPermission('ADMINISTRATOR'))
+    .add(2, (message) => message.author.id === message.guild.ownerID)
+    .add(9, (message) => message.client.owners.has(message.author), {
+      break: true,
+    })
+    .add(10, (message) => message.client.owners.has(message.author)),
   prefix: '!',
   readyMessage: (client: KlasaClient) =>
     `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`,
+  slowmode: 2000,
+  slowmodeAggressive: true,
   typing: true,
 });
 
