d949f6367a1cc9ba6875557e95d46862c5333cf7
Skillz <gankstarsleague@gmail.com>
2019-07-21 14:23:21 -0400

    fix bugs in ayah and optimize for multiple ayahs

--- commands/Quran/ayah.ts
+++ commands/Quran/ayah.ts
@@ -11,33 +11,79 @@ export default class extends Command {
   constructor(store: CommandStore, file: string[], directory: string) {
     super(store, file, directory, {
       aliases: ['a'],
+      cooldown: 5,
       description: (language) => language.get('AYAH_DESCRIPTION'),
+      promptLimit: Infinity,
       quotedStringSupport: true,
       requiredPermissions: [Permissions.FLAGS.EMBED_LINKS],
-      usage: '[surah:str] [ayah:str]',
+      usage: '(surah:surah) [ayah:integer{1,293}] [endingAyah:integer]',
       usageDelim: ' ',
     });
+
+    this.createCustomResolver(
+      `surah`,
+      async(arg, _possible, message: KlasaMessage) => {
+        if (!arg) {
+          await message.send(`Finding random verse since none was provided.`);
+          return this.getRandom();
+        }
+        // If something was provided check if its a valid surah
+        const surah = Quran[`surah_${this.findSurah(arg)}`];
+        if (!surah)
+          throw `I was not able to find ${arg} surah. If you believe this is an mistake, please contact me on my server using the **invite** command.`;
+        // The surah is valid so return the argument
+        return arg;
+      }
+    );
   }
 
-  async run(message: KlasaMessage, [surah, ayah]: [string | undefined, string]) {
-    const surahToUse = Quran[`surah_${this.findSurah(surah)}`];
-    const ayahToSend = surahToUse[`ayah_${ayah}` || this.getRandom(surah)];
+  async run(
+    message: KlasaMessage,
+    [surahName, ayahNumber, endAyah]: [
+      string,
+      number | undefined,
+      number | undefined
+    ]
+  ) {
+    // Check if the end ayah is valid
+    if (endAyah && endAyah < ayahNumber)
+      return message.send(
+        `You can't have a ending ayah number smaller then the starting ayah number.`
+      );
+    // if trying to quote too many prevent spam
+    if (endAyah - ayahNumber > 10 && !message.hasAtLeastPermissionLevel(1)) return message.send(`You can't have that many ayah sent at once please try a smaller range. Must be less than 10.`);
 
-    return message.send(
-      new MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor(
-          message.language.get(
-            'AYAH_SURAH_AND_AYAH',
-            surahToUse.name,
-            ayah
-          ),
-          message.author.displayAvatarURL()
-        )
-        .setDescription(ayahToSend.text)
-        .setImage(ayahToSend.image)
-        .setFooter(message.language.get('QURAN_COM_CREDITS'))
-    );
+    const surahToFind = `surah_${this.findSurah(surahName)}`;
+    const surahToUse = Quran[surahToFind];
+
+    for (let i = ayahNumber; i <= endAyah; i++) {
+      const ayahToSend =
+        surahToUse[`ayah_${i}` || this.getRandom(surahToUse)];
+      if (!ayahToSend) {
+        await message.channel.send(
+          `Are you sure you provided a valid ayah number? I can't find ${i} ayah for surah ${
+            surahToUse.name
+          }`
+        );
+        break;
+      }
+
+      await message.channel.send(
+        new MessageEmbed()
+          .setColor('RANDOM')
+          .setAuthor(
+            message.language.get(
+              'AYAH_SURAH_AND_AYAH',
+              surahToUse.name,
+              ayahNumber
+            ),
+            message.author.displayAvatarURL()
+          )
+          .setDescription(ayahToSend.text)
+          .setImage(ayahToSend.image)
+          .setFooter(message.language.get('QURAN_COM_CREDITS'))
+      );
+    }
   }
 
   findSurah(surah: string) {
