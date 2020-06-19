43efad622d2b1f595e1eb202e3c282e551cd6d6c
Skillz <gankstarsleague@gmail.com>
2020-06-18 21:08:20 -0400

    test fix

--- src/commands/Quran/remind.ts
+++ src/commands/Quran/remind.ts
@@ -15,16 +15,18 @@ export default class extends Command {
   }
 
   async run(message: Message) {
-    const channelID = message.guild!.settings.get(
+    const channelID = message.guild?.settings.get(
       GuildSettings.FinishMonthlyChannelID
     ) as GuildSettings.ChannelID | null;
     if (!channelID) return message.sendLocale(`REMIND_NOT_SETUP`);
 
-    const enabled = message.author.settings.get(UserSettings.FinishMonthlyEnabled);
+    await message.author.settings.sync()
+    
+    const enabled = message.author.settings.get(UserSettings.FinishMonthlyEnabled) as boolean;
 
     await message.author.settings.update(UserSettings.FinishMonthlyEnabled, !enabled);
 
-    await message.guild!.settings.update(GuildSettings.FinishMonthlyUserIDs, message.author.id);
+    await message.guild!.settings.update(GuildSettings.FinishMonthlyUserIDs, message.author.id, { arrayAction: "auto" });
 
     return message.sendLocale(enabled ? "REMIND_DISABLED" : "REMIND_ENABLED");
   }
