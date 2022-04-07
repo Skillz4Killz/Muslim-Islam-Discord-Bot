2c39aa677d9dd480f65f29a43f98fe979ddd6ef6
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-04-07 10:29:56 +0000

    fix: use latest discordeno

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export * from "https://deno.land/std@0.125.0/fmt/colors.ts";
-export * from "https://deno.land/x/discordeno@13.0.0-rc18/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc34/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.5/mod.ts";
 export * from "./cache.ts";
